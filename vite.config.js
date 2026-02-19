95a40ff5dbb35fe05925e82287221c184aac69e6
abhi293 <anandabhinav293@gmail.com>
2026-02-20 00:08:26 +0530

    fist commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/computer_vision_tutorials/", // Replace with your exact repository name
 })
